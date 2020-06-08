ccf1288cf867cb56a7a70a1d9bd184f28565236f
gpeng <michael.gang.peng@gmail.com>
2020-06-08 17:38:24 -0400

    Initial Commit

---
+++ fstPair/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fstPair
+//
+//  Created by Gang Peng on 6/8/20.
+//  Copyright © 2020 Gang Peng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
